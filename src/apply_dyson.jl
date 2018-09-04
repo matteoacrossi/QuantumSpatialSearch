735c269835f62a93867353ef669648dba2a38804
Matteo Rossi <teo.red90@gmail.com>
2018-09-04 14:54:33 +0300

    Small refactoring of apply_dyson

--- src/apply_dyson.jl
+++ src/apply_dyson.jl
@@ -1,9 +1,7 @@
-function apply_dyson!(H, psi, dt, dysonOrder)
+function apply_dyson!(H, psi, dt::Real, dysonOrder::Integer)
     kq = copy(psi)
     tmp = similar(kq)
-    # evolution (dysonOrder is the maximum order of dt)
     for k = 1 : dysonOrder
-        #kq = - 1im * dt/k * H * kq
         mul!(tmp, H, kq, -1im * dt/k, 0)
         psi .+= tmp
         copyto!(kq, tmp)
