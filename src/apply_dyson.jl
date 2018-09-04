96a8185e29b7aae64fa332f2b55d6b54bee55da5
Matteo Rossi <teo.red90@gmail.com>
2018-09-04 14:31:01 +0300

    Dyson performance improvement

--- src/apply_dyson.jl
+++ src/apply_dyson.jl
@@ -1,8 +1,11 @@
 function apply_dyson!(H, psi, dt, dysonOrder)
     kq = copy(psi)
+    tmp = similar(kq)
     # evolution (dysonOrder is the maximum order of dt)
     for k = 1 : dysonOrder
-        kq = - 1im * dt/k * H * kq
-        psi .+= kq
+        #kq = - 1im * dt/k * H * kq
+        mul!(tmp, H, kq, -1im * dt/k, 0)
+        psi .+= tmp
+        copyto!(kq, tmp)
     end
 end
