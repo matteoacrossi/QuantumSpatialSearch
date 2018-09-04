429285fa62fa16658e55b41538ef4320ab83c4f1
Matteo Rossi <teo.red90@gmail.com>
2018-09-04 14:24:52 +0300

    Fix?

--- src/apply_dyson.jl
+++ src/apply_dyson.jl
@@ -1,8 +1,8 @@
-function dyson_evolution!(H, psi, dt, dysonOrder)
+function apply_dyson!(H, psi, dt, dysonOrder)
     kq = copy(psi)
     # evolution (dysonOrder is the maximum order of dt)
     for k = 1 : dysonOrder
         kq = - 1im * dt/k * H * kq
-        psi += kq
+        psi .+= kq
     end
 end
